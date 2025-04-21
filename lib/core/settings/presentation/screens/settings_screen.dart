ee2badb0728a0242eebb723ee71cb70bb57d9e1a
DELL <ytykhn8@gmail.com>
2025-04-21 18:08:03 +0300

    fix

--- lib/core/settings/presentation/screens/settings_screen.dart
+++ lib/core/settings/presentation/screens/settings_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("הגדרות")),
