8c349bb054fde2ea07b4565f7c5b74689786d831
DELL <ytykhn8@gmail.com>
2025-04-30 13:41:18 +0300

    Add logic for login

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -1,7 +1,19 @@
+import 'dart:convert';
+
 import 'package:driver_app/features/auth/domain/entities/user.dart';
+import 'package:http/http.dart' as http;
 
 class AuthRemoteDatasource {
-  Future<User> login(String email, String password) async {
-    return User(email: email, id: "1");
+  Future<User> login(String username, String id) async {
+    final response = await http.post(
+      Uri.parse("localhost:5000/login"),
+      body: json.encode({'username': username , 'id': id })
+    );
+    if (response.statusCode == 200){
+      final data = json.decode(response.body);
+      return User(userName: data["username"], id:data["id"]);
+    }
+    
+    return User(userName: username, id: "1");
   }
 }
