6d1e191a8e98867099797238d848aef0aeeda38a
Yair <ytykhn8@gmail.com>
2025-04-30 04:13:24 -0700

    Add connect to the server

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -5,15 +5,19 @@ import 'package:http/http.dart' as http;
 
 class AuthRemoteDatasource {
   Future<User> login(String username, String id) async {
-    final response = await http.post(
-      Uri.parse("localhost:5000/login"),
-      body: json.encode({'username': username , 'id': id })
-    );
-    if (response.statusCode == 200){
-      final data = json.decode(response.body);
-      return User(userName: data["username"], id:data["id"]);
+    try {
+      final response = await http.post(
+        Uri.parse("http://127.0.0.1:5000/login"),
+        body: json.encode({'username': username, 'id': id}),
+      );
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return User(userName: data["username"], id: data["id"]);
+      }
+
+      return Future.error("שגיאת שרת");
+    } catch (e) {
+      return Future.error("שגיאת תקשורת");
     }
-    
-    return User(userName: username, id: "1");
   }
 }
