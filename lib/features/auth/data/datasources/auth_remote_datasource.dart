76be636df5a666162be67f4aba96c3bfd4115d62
DELL <ytykhn8@gmail.com>
2025-05-04 23:02:52 +0300

    add logic for jwt

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -8,16 +8,28 @@ class AuthRemoteDatasource {
     try {
       final response = await http.post(
         Uri.parse("http://127.0.0.1:5000/login"),
+        headers: {'Content-Type': 'application/json'},
         body: json.encode({'username': username, 'id': id}),
       );
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        return User(userName: data["username"], id: data["id"]);
-      }
 
-      return Future.error("שגיאת שרת");
+      if (response.statusCode == 201) {
+        final data = json.decode(response.body);
+        return User(userName: data["username"], id: data["id"],token: data["token"]);
+      } else if (response.statusCode == 403) {
+        final error = json.decode(response.body);
+        return Future.error(error["error"] ?? "משתמש לא מורשה");
+      } else {
+        return Future.error("שגיאה כללית בשרת (${response.statusCode})");
+      }
     } catch (e) {
-      return Future.error("שגיאת תקשורת");
+      return Future.error("שגיאת תקשורת: $e");
     }
+  }
+  Future<bool> validateToken(String token) async {
+    final response = await http.get(Uri(host: "localhost", port: 5000,path: "api/login/check-token"));
+    if (response.statusCode == 200){
+      return true;
+    }
+    return false;
   }
 }
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,4 +2,6 @@ import 'package:driver_app/features/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
   Future<User> login(String email, String password);
+  Future<bool> validateSavedToken(String token);
+  Future<String> getSavedToken();
 }
